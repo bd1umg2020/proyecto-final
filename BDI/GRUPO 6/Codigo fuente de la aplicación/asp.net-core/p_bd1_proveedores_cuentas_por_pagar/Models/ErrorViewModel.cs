7fae178791a2af4101419abebc72f8b6c3ba10d9
Carlos Ambeliz <carlosambeliz@hotmail.com>
2020-10-23 14:54:31 -0600

    incorporacion del proyecto desarrollado en asp.net core

---
+++ BDI/GRUPO 6/Codigo fuente de la aplicación/asp.net-core/p_bd1_proveedores_cuentas_por_pagar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace p_bd1_proveedores_cuentas_por_pagar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
